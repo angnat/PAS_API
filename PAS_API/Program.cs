9f39377dc2bbaffa58a4363c8fe340501eb582d2
angnat <angnat05@yahoo.com>
2023-12-02 10:10:19 +0700

    biar bs d panggil dari luar

--- PAS_API/Program.cs
+++ PAS_API/Program.cs
@@ -23,15 +23,16 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.WebHost.UseUrls("http://0.0.0.0:5001/");
+//builder.WebHost.UseIISIntegration();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 
 app.UseHttpsRedirection();
 
